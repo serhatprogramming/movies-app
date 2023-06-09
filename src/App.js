6b8f8cb4997ce74465414ea31525f5939ba41a91
serhatprogramming <serhatprogramming@gmail.com>
2023-06-09 10:39:42 -0400

    finalized.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import Movie from "./components/Movie";
+
 const App = ({ movies }) => {
   return (
     <div>
       <h1>Must Watch Movies</h1>
       <ul>
         {movies.map((movie) => (
-          <li key={movie.id}>{movie.title}</li>
+          <Movie key={movie.id} movie={movie} />
         ))}
       </ul>
     </div>
