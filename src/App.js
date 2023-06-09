7f5d21db5e3d51344f3bcbe30b51c483c53947c5
serhatprogramming <serhatprogramming@gmail.com>
2023-06-09 10:23:33 -0400

    list with key

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-import React from "react";
-
-const App = () => {
-  return <div>Hello</div>;
+const App = ({ movies }) => {
+  return (
+    <div>
+      <h1>Must Watch Movies</h1>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,5 +3,23 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
+const movies = [
+  {
+    id: 1,
+    title: "The Godfather (1972)",
+    watchList: true,
+  },
+  {
+    id: 2,
+    title: "The Shawshank Redemption (1994)",
+    watchList: false,
+  },
+  {
+    id: 3,
+    title: "The Lord of the Rings: The Return of the King (2003)",
+    watchList: true,
+  },
+];
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<App movies={movies} />);
