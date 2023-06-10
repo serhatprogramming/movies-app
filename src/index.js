bfeec11e0f44bb15d5af6d15fc12f7f3f76d4d93
serhatprogramming <serhatprogramming@gmail.com>
2023-06-09 20:48:16 -0400

    .

--- src/index.js
+++ src/index.js
@@ -6,17 +6,17 @@ import App from "./App";
 const movies = [
   {
     id: 1,
-    title: "The Godfather",
+    title: "The Godfather (1972)",
     watchList: true,
   },
   {
     id: 2,
-    title: "The Shawshank Redemption",
+    title: "The Shawshank Redemption (1994)",
     watchList: false,
   },
   {
     id: 3,
-    title: "The Lord of the Rings: The Return of the King",
+    title: "The Lord of the Rings: The Return of the King (2003)",
     watchList: true,
   },
 ];
